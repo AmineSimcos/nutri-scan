3dba26083f175c88e917a35f2f95cad33b55923a
WDD Fall 17 <emilyzhongsihua@gmail.com>
2018-01-20 22:07:50 -0800

    bars

--- app/src/main/java/com/nutriscan/nutri_scan/Dashboard.java
+++ app/src/main/java/com/nutriscan/nutri_scan/Dashboard.java
@@ -27,13 +27,28 @@ public class Dashboard extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                // TODO: complete this.
+                Snackbar.make(view, "Add ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+        // TODO: Replace fillers.
         int caloriesConsumed = 400;
         int caloriesLeft = 1600;
+
+        int regularCarbs = 200;
+        int naturalSugars = 100;
+        int addedSugars = 50;
+        int carbsLeft = 20;
+
+        int protein = 300;
+        int proteinLeft = 400;
+
+        int transFat = 20;
+        int saturatedFat = 10;
+        int otherFats = 30;
+        int fatsLeft = 5;
 
         WebView webview = (WebView) findViewById(R.id.webView1);
         String content = "<html>"
@@ -41,8 +56,12 @@ public class Dashboard extends AppCompatActivity {
                 + "    <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>"
                 + "    <script type=\"text/javascript\">"
                 + "      google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});"
-                + "      google.setOnLoadCallback(drawChart);"
-                + "      function drawChart() {"
+                + "      google.setOnLoadCallback(drawCharts);"
+
+                + "      function drawCharts() {"
+                + "         drawPieChart(); drawBarCharts(); }"
+
+                + "      function drawPieChart() {"
                 + "        var data = google.visualization.arrayToDataTable(["
                 + "          ['Category', 'Calories'], "
                 + "          ['Calories Consumed'," + caloriesConsumed + "],"
@@ -59,11 +78,65 @@ public class Dashboard extends AppCompatActivity {
                 + "        var chart = new google.visualization.PieChart(document.getElementById('calories_pie'));"
                 + "        chart.draw(data, options);"
                 + "      }"
+
+                + "     function drawBarCharts() {" +
+                "           var options = {" +
+                "               isStacked: 'percent'," +
+                "               vAxis: {" +
+//                "                   gridlines: {color: 'transparent'}," +
+//                "                   textPosition: 'none'," +
+                "                       count: 2" +
+                "               }," +
+                "               hAxis: {" +
+                "                   gridlines: {color: 'transparent'}," +
+                "                   baseline: {color: 'transparent'}," +
+                "                   textPosition: 'none'" +
+                "               }," +
+                "               legend: {position: 'bottom', maxLines: 1}," +
+                "               chartArea: {width: '100%', height: '50%'}" +
+                "            };" +
+
+                "           var carbsData = google.visualization.arrayToDataTable([" +
+                "               ['', 'Regular', 'Natural Sugars', 'Added Sugars', 'Remaining', ], " +
+                "               [''," + regularCarbs + "," + naturalSugars + "," + addedSugars + "," + carbsLeft + "]" +
+                "           ]);" +
+
+                "           var proteinData = google.visualization.arrayToDataTable([" +
+                "               ['', 'Consumed', 'Remaining'], " +
+                "               [''," + protein + "," + proteinLeft + "]" +
+                "           ]);" +
+
+                "           var fatsData = google.visualization.arrayToDataTable([" +
+                "               ['', 'Trans Fat', 'Saturated Fat', 'Other', 'Remaining', ], " +
+                "               [''," + transFat + "," + saturatedFat + "," + otherFats + "," + fatsLeft + "]" +
+                "           ]);" +
+
+                "           var carbsChart = new google.visualization.BarChart(document.getElementById('carbs'));" +
+                "           carbsChart.draw(carbsData, options);" +
+
+                "           var proteinChart = new google.visualization.BarChart(document.getElementById('protein'));" +
+                "           proteinChart.draw(proteinData, options);" +
+
+                "           var fatsChart = new google.visualization.BarChart(document.getElementById('fats'));" +
+                "           fatsChart.draw(fatsData, options);" +
+                "       }"
                 + "    </script>"
                 + "  </head>"
-                + "  <body>"
+                + "  <body>" +
+                "       <style>" +
+                "           h3 { margin-top: 5vh;}" +
+                "       </style>"
                 + "    <h3 style=\"text-align: center\">Today's Caloric Consumption</h3>"
                 + "    <div id=\"calories_pie\"></div>"
+
+                + "    <h3 style=\"text-align: center\">Carbohydrate Consumption</h3>"
+                + "    <div id=\"carbs\"></div>"
+
+                + "    <h3 style=\"text-align: center\">Protein Consumption</h3>"
+                + "    <div id=\"protein\"></div>"
+
+                + "    <h3 style=\"text-align: center\">Fat Consumption</h3>"
+                + "    <div id=\"fats\"></div>"
                 + "  </body>" + "</html>";
 
         WebSettings webSettings = webview.getSettings();
